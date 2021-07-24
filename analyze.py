37dc478708ff5e1d26ba4aef494ad01122a84738
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2021-07-24 15:00:22 +0200

    Structured added gitignore

--- analyze.py
+++ analyze.py
@@ -2,7 +2,8 @@ import codecs
 import re
 import pandas
 
-if __name__ == "__main__":
+
+def create_data_frame():
     with codecs.open('text.txt', encoding='utf-8') as f:
         years = []
         months = []
@@ -49,40 +50,49 @@ if __name__ == "__main__":
         df = pandas.DataFrame({"Year": years, "Month": months, "Day": days, "Hour": hours, "Minute": minutes,
                                "Names": names, "Messages": messages, "Media": media, "Word Count": word_counts,
                                "Length": lengths})
+        return df
 
-        with open('analysis.txt', 'w') as f:
-            f.write("---MESSAGES---\n")
-            f.write(f"Message Count: {df['Messages'].size}\n")
-            f.write(f"Lukas Message Count: {df[df['Names'] == 'Lukas']['Messages'].size}\n")
-            f.write(f"Lukas Message Count: {df[df['Names'] == 'Lukas']['Messages'].size/df['Messages'].size*100}%\n")
-            f.write(f"Tobias Message Count: {df[df['Names'] == 'Tobias Jungbluth']['Messages'].size}\n")
-            f.write(f"Tobias Message Count: {df[df['Names'] == 'Tobias Jungbluth']['Messages'].size/df['Messages'].size*100}%\n")
 
-            f.write("---WORD COUNT---\n")
-            f.write(f"Word Count : {df['Word Count'].sum()}\n")
-            f.write(f"Word Count Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Word Count'].sum()}\n")
-            f.write(f"Word Count Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Word Count'].sum()/df['Word Count'].sum() * 100}%\n")
-            f.write(f"Word Count Lukas: {df[df['Names'] == 'Lukas']['Word Count'].sum()}\n")
-            f.write(f"Word Count Lukas: {df[df['Names'] == 'Lukas']['Word Count'].sum()/df['Word Count'].sum() * 100}%\n")
-            f.write(f"Word Count Mean: {df['Word Count'].mean()}\n")
-            f.write(f"Word Count Tobi Mean: {df[df['Names'] == 'Tobias Jungbluth']['Word Count'].mean()}\n")
-            f.write(f"Word Count Lukas Mean: {df[df['Names'] == 'Lukas']['Word Count'].mean()}\n")
+def out_put_analysis(df):
+    with open('analysis.txt', 'w') as f:
+        f.write("---MESSAGES---\n")
+        f.write(f"Message Count: {df['Messages'].size}\n")
+        f.write(f"Lukas Message Count: {df[df['Names'] == 'Lukas']['Messages'].size}\n")
+        f.write(f"Lukas Message Count: {df[df['Names'] == 'Lukas']['Messages'].size/df['Messages'].size*100}%\n")
+        f.write(f"Tobias Message Count: {df[df['Names'] == 'Tobias Jungbluth']['Messages'].size}\n")
+        f.write(
+            f"Tobias Message Count: {df[df['Names'] == 'Tobias Jungbluth']['Messages'].size/df['Messages'].size*100}%\n")
 
-            f.write("---WORD LENGTH---\n")
-            f.write(f"Word Length : {df['Length'].sum()}\n")
-            f.write(f"Word Length Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Length'].sum()}\n")
-            f.write(f"Word Length Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Length'].sum()/df['Length'].sum() * 100}%\n")
-            f.write(f"Word Length Lukas: {df[df['Names'] == 'Lukas']['Length'].sum()}\n")
-            f.write(f"Word Length Lukas: {df[df['Names'] == 'Lukas']['Length'].sum()/df['Length'].sum() * 100}%\n")
-            f.write(f"Word Length Mean: {df['Length'].mean()}\n")
-            f.write(f"Word Length Tobi Mean: {df[df['Names'] == 'Tobias Jungbluth']['Length'].mean()}\n")
-            f.write(f"Word Length Lukas Mean: {df[df['Names'] == 'Lukas']['Length'].mean()}\n")
+        f.write("---WORD COUNT---\n")
+        f.write(f"Word Count : {df['Word Count'].sum()}\n")
+        f.write(f"Word Count Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Word Count'].sum()}\n")
+        f.write(
+            f"Word Count Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Word Count'].sum()/df['Word Count'].sum() * 100}%\n")
+        f.write(f"Word Count Lukas: {df[df['Names'] == 'Lukas']['Word Count'].sum()}\n")
+        f.write(f"Word Count Lukas: {df[df['Names'] == 'Lukas']['Word Count'].sum()/df['Word Count'].sum() * 100}%\n")
+        f.write(f"Word Count Mean: {df['Word Count'].mean()}\n")
+        f.write(f"Word Count Tobi Mean: {df[df['Names'] == 'Tobias Jungbluth']['Word Count'].mean()}\n")
+        f.write(f"Word Count Lukas Mean: {df[df['Names'] == 'Lukas']['Word Count'].mean()}\n")
 
-            f.write("---MEDIA---\n")
-            f.write(f"Non Media Messages: {df['Messages'].count()}\n")
-            f.write(f"Non Media Messages: {df['Messages'].count()/df['Messages'].size*100}%\n")
-            f.write(f"Media Messages: {df['Messages'].size - df['Messages'].count()}\n")
-            f.write(f"Media Messages: {(df['Messages'].size - df['Messages'].count())/df['Messages'].size*100}%\n")
-            f.write(f"Is Media Tobi: {df[(df['Media'] == True) & (df['Names'] == 'Tobias Jungbluth')]['Media'].count()}\n")
-            f.write(f"Is Media Tobi: {df[(df['Media'] == True) & (df['Names'] == 'Tobias Jungbluth')]['Media'].count()}\n")
+        f.write("---WORD LENGTH---\n")
+        f.write(f"Word Length : {df['Length'].sum()}\n")
+        f.write(f"Word Length Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Length'].sum()}\n")
+        f.write(
+            f"Word Length Tobi: {df[df['Names'] == 'Tobias Jungbluth']['Length'].sum()/df['Length'].sum() * 100}%\n")
+        f.write(f"Word Length Lukas: {df[df['Names'] == 'Lukas']['Length'].sum()}\n")
+        f.write(f"Word Length Lukas: {df[df['Names'] == 'Lukas']['Length'].sum()/df['Length'].sum() * 100}%\n")
+        f.write(f"Word Length Mean: {df['Length'].mean()}\n")
+        f.write(f"Word Length Tobi Mean: {df[df['Names'] == 'Tobias Jungbluth']['Length'].mean()}\n")
+        f.write(f"Word Length Lukas Mean: {df[df['Names'] == 'Lukas']['Length'].mean()}\n")
 
+        f.write("---MEDIA---\n")
+        f.write(f"Non Media Messages: {df['Messages'].count()}\n")
+        f.write(f"Non Media Messages: {df['Messages'].count()/df['Messages'].size*100}%\n")
+        f.write(f"Media Messages: {df['Messages'].size - df['Messages'].count()}\n")
+        f.write(f"Media Messages: {(df['Messages'].size - df['Messages'].count())/df['Messages'].size*100}%\n")
+        f.write(f"Is Media Tobi: {df[(df['Media'] == True) & (df['Names'] == 'Tobias Jungbluth')]['Media'].count()}\n")
+        f.write(f"Is Media Tobi: {df[(df['Media'] == True) & (df['Names'] == 'Tobias Jungbluth')]['Media'].count()}\n")
+
+
+if __name__ == "__main__":
+    out_put_analysis(create_data_frame())
